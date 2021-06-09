783a5f20a2c7bdfd09c41eb7af60b56f809c9bf0
Denuk68 <denus.denisovuch@gmail.com>
2021-06-09 14:37:20 +0300

    added cart

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 // Trucks hover effect
-let trucks_card = document.querySelectorAll(".trucks .card")
+let trucks_card = document.querySelectorAll(".trucks .card");
 let trucks_card_h1 = document.querySelectorAll(".trucks .card h1");
 let trucks_card_img = document.querySelectorAll(".trucks .card  img");
 
-for(let i = 0; i <= trucks_card.length; i++){
-  
+for(let i = 0; i < trucks_card.length; i++){
+    trucks_card[i].addEventListener('mouseenter',function(){
+        trucks_card[i].classList.add('hover');
+        trucks_card_h1[i].classList.add('hover');
+        trucks_card_img[i].classList.add('hover');
+    })
+};
+for(let i = 0; i < trucks_card.length; i++){
+    trucks_card[i].addEventListener('mouseleave',function(){
+        trucks_card[i].classList.remove('hover');
+        trucks_card_h1[i].classList.remove('hover');
+        trucks_card_img[i].classList.remove('hover');
+    })
 };
