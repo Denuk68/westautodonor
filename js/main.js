e0371995cbd99f921a3f8ec91eb0e41ad516a109
Denuk68 <denus.denisovuch@gmail.com>
2021-06-09 12:39:59 +0300

    First push

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Trucks hover effect
+let trucks_card = document.querySelectorAll(".trucks .card")
+let trucks_card_h1 = document.querySelectorAll(".trucks .card h1");
+let trucks_card_img = document.querySelectorAll(".trucks .card  img");
+
+for(let i = 0; i <= trucks_card.length; i++){
+  
+};
